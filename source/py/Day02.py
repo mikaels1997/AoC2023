d03b269e3bb109c5b8db7704aab3517364931266
Mikael Särkiniemi <mikaels1997@gmail.com>
2023-12-11 02:13:27 +0200

    Day2

---
+++ source/py/Day02.py
@@ -0,0 +1,28 @@
+def solve(path, second_part = False):
+    limits = {"red": 12, "green": 13, "blue": 14}
+    result = 0
+    with open(path) as f:
+        for line in f.readlines():
+            game_id = int(line.split(" ")[1][:-1])
+            games = line.split(':')[-1].split(';')
+            invalid = False
+            max_nums = {"red": 0, "green": 0, "blue": 0}
+            for game in games:
+                cubes = game.strip().split(',')
+                for cube in cubes:
+                    parsed = cube.strip().split(" ")
+                    num, color = int(parsed[0]), parsed[1].strip()
+                    if max_nums[color] < num:
+                        max_nums[color] = num
+                    if limits[color] >= num and not second_part:
+                        continue
+                    invalid = True
+            if second_part:
+                result += max_nums["red"] * max_nums["green"] * max_nums["blue"]
+            if not invalid and not second_part:
+                result += game_id              
+    return result
+
+if __name__ == "__main__":
+    print("Part 1 solution: ", solve("data/input02.txt"))
+    print("Part 2 solution: ", solve("data/input02.txt", True))
